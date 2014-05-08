7cc3622b37d139e3c074acf399b4d540051380bd
brettyukich <brettyukich@gmail.com>
2014-05-08 20:50:12 +1200

    created basic server api using mux and mongodb

--- radegast.go
+++ radegast.go
@@ -1,17 +1,151 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
-	"os"
+	"time"
+
+	"log"
+
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/", fs)
+var (
+	session    *mgo.Session
+	collection *mgo.Collection
+)
 
-	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+type Recipe struct {
+	Id      bson.ObjectId `bson:"_id" json:"id"`
+	Name    string        `json:"name"`
+	Created time.Time     `json:"created"`
+}
+
+type RecipeJSON struct {
+	Recipe Recipe `json:"recipe"`
+}
+
+type RecipesJSON struct {
+	Recipes []Recipe `json:"recipes"`
+}
+
+func main() {
+
+	var err error
+
+	log.Println("Starting Server")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/api/recipes/{id:[0-9]+}", RetrieveRecipeHandler).Methods("GET")
+	r.HandleFunc("/api/recipes/{id:[0-9]+}", UpdateRecipeHandler).Methods("PUT")
+	r.HandleFunc("/api/recipes/{id:[0-9]+}", DeleteRecipeHandler).Methods("DELETE")
+	r.HandleFunc("/api/recipes/", CreateRecipeHandler).Methods("POST")
+	http.Handle("/api/", r)
+	http.Handle("/", http.FileServer(http.Dir(".")))
+
+	log.Println("Starting mongo db session")
+	// MongoDB
+	session, err = mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	collection = session.DB("radegast").C("recipes")
+
+	log.Println("Listening on 8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func CreateRecipeHandler(w http.ResponseWriter, r *http.Request) {
+	var recipeJSON RecipeJSON
+
+	err := json.NewDecoder(r.Body).Decode(&recipeJSON)
 	if err != nil {
 		panic(err)
 	}
 
+	recipe := recipeJSON.Recipe
+
+	obj_id := bson.NewObjectId()
+	recipe.Id = obj_id
+	recipe.Name = "test"
+	recipe.Created = time.Now().Local()
+
+	err = collection.Insert(&recipe)
+	if err != nil {
+		panic(err)
+	} else {
+		log.Printf("Inserted new recipe %s with name %s", recipe.Id, recipe.Name)
+	}
+
+	j, err := json.Marshal(RecipeJSON{Recipe: recipe})
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(j)
+}
+
+func RetrieveRecipeHandler(w http.ResponseWriter, r *http.Request) {
+
+	var recipes []Recipe
+
+	iter := collection.Find(nil).Iter()
+	result := Recipe{}
+	for iter.Next(&result) {
+		recipes = append(recipes, result)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	j, err := json.Marshal(RecipesJSON{Recipes: recipes})
+	if err != nil {
+		panic(err)
+	}
+	w.Write(j)
+	log.Println("provided json")
+}
+
+func UpdateRecipeHandler(w http.ResponseWriter, r *http.Request) {
+	var err error
+	// Gran the recipe id from the incoming url
+	vars := mux.Vars(r)
+	id := bson.ObjectIdHex(vars["id"])
+
+	// Decode the indoming recipe json
+	var recipeJSON RecipeJSON
+	err = json.NewDecoder(r.Body).Decode(&recipeJSON)
+	if err != nil {
+		panic(err)
+	}
+
+	//Update the database
+	err = collection.Update(bson.M{"_id": id},
+		bson.M{"name": recipeJSON.Recipe.Name,
+			"_id":     id,
+			"created": recipeJSON.Recipe.Created,
+		})
+	if err == nil {
+		log.Printf("Updated recipe %s name to %s", id, recipeJSON.Recipe.Name)
+	} else {
+		panic(err)
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func DeleteRecipeHandler(w http.ResponseWriter, r *http.Request) {
+	// Gran the recipe's id from the incoming url
+	var err error
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	// Remove it from database
+	err = collection.Remove(bson.M{"_id": bson.ObjectIdHex(id)})
+	if err != nil {
+		log.Printf("Could not find recipe %s to delete", id)
+	}
+	w.WriteHeader(http.StatusNoContent)
 }
