f0051287a3b2f1a71678295ac9ddae748af2a0fc
brettyukich <brettyukich@gmail.com>
2014-05-10 16:11:55 +1200

    big refactor of js, got link to recipes working

---
+++ static/js/radegastController.js
@@ -0,0 +1,45 @@
+'use strict';
+
+radegastApp.controller('MainCtrl', ['$scope', '$route', '$routeParams', '$location',
+	function ($scope, $route, $routeParams, $location) {
+		$scope.$route = $route;
+		$scope.$location = $location;
+		$scope.$routeParams = $routeParams;
+	}
+]);
+
+radegastApp.controller('RecipeListCtrl', ['$scope', '$http', '$routeParams',
+	function ($scope, $http, $routeParams) {
+
+		$scope.routeParams = $routeParams;
+		$scope.recipes = [];
+
+		$scope.getRecipes = function () {
+			$http({method: 'GET', url: 'http://localhost:8080/api/recipes'}).
+				success(function (data, status, headers, config) {
+				$scope.recipes = data.recipes;
+				}).error(function (data, status) {
+			});
+		};
+
+		$scope.getRecipes();
+	}
+]);
+
+radegastApp.controller('RecipeItemCtrl', ['$scope', '$http', '$routeParams',
+	function ($scope, $http, $routeParams) {
+
+		$scope.routeParams = $routeParams;
+		$scope.recipe = null;
+
+		$scope.getRecipe = function () {
+			$http({method: 'GET', url: 'http://localhost:8080/api/recipes/' + $routeParams.id}).
+				success(function (data, status, headers, config) {
+				$scope.recipeName = data.recipe;
+				}).error(function () {
+			});
+		};
+
+		$scope.getRecipe();
+	}
+]);
